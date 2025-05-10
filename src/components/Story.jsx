e00afaa7ef4a7b3f0bebb900e5b1ec6695452fb5
farithk <farithcomas@Fariths-Mac-mini.local>
2025-05-09 19:58:38 -0500

    fixing usenavigate issue

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import CircularProgress from '@mui/material/CircularProgress';
